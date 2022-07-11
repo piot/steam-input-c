e1c3f92ef009d3ab175580525f1e05d76ccadb6e
Peter Bjorklund <piot@hotmail.com>
2022-07-11 20:58:07 +0200

    chore: still not working

--- src/include/stik/stik.h
+++ src/include/stik/stik.h
@@ -3,15 +3,64 @@
 
 #include <steam-api/steam_api.h>
 
+typedef SteamU64 InputHandle_t;
+typedef SteamU64 InputActionSetHandle_t;
+typedef SteamU64 InputDigitalActionHandle_t;
+
+typedef struct InputDigitalActionData_t
+{
+//#pragma pack(push, 0)
+    //SteamBool state;
+    //SteamBool active;
+//#pragma pack(pop)
+uint64_t data1;
+uint64_t data2;
+} InputDigitalActionData_t;
+
+typedef SteamInt EInputActionOrigin;
+typedef SteamInt ESteamInputType;
 typedef void* ISteamInput;
+
+typedef ISteamInput* (S_CALLTYPE *SteamAPI_SteamInput_v006)(void);
+typedef SteamBool (S_CALLTYPE *SteamAPI_ISteamInput_Init)(ISteamInput* input);
+typedef InputActionSetHandle_t (S_CALLTYPE *SteamAPI_ISteamInput_GetActionSetHandle)(ISteamInput* input, SteamConstantString actionSet);
+typedef InputDigitalActionHandle_t (S_CALLTYPE *SteamAPI_ISteamInput_GetDigitalActionHandle)(ISteamInput* input, SteamConstantString digitalAction);
+typedef InputHandle_t (S_CALLTYPE *SteamAPI_ISteamInput_GetControllerForGamepadIndex)(ISteamInput* input, SteamInt controllerIndex);
+typedef void (S_CALLTYPE *SteamAPI_ISteamInput_ActivateActionSet)(ISteamInput* input, InputHandle_t inputHandle, InputActionSetHandle_t actionSetHandle);
+typedef SteamInt (S_CALLTYPE *SteamAPI_ISteamInput_GetConnectedControllers)(ISteamInput* input, InputHandle_t *handlesOut);
+typedef void (S_CALLTYPE *SteamAPI_ISteamInput_RunFrame)(ISteamInput* input);
+typedef SteamInt (S_CALLTYPE *SteamAPI_ISteamInput_GetDigitalActionOrigins)(ISteamInput* input, InputHandle_t inputHandle, InputActionSetHandle_t actionSetHandle, InputDigitalActionHandle_t digitalActionHandle, EInputActionOrigin *originsOut);
+typedef InputDigitalActionData_t (S_CALLTYPE *SteamAPI_ISteamInput_GetDigitalActionData)(ISteamInput* input, InputHandle_t controllerHandle, SteamU64 digitalActionHandle);
+typedef InputActionSetHandle_t (S_CALLTYPE *SteamAPI_ISteamInput_GetCurrentActionSet)(ISteamInput* input, InputHandle_t controllerHandle);
+typedef ESteamInputType (S_CALLTYPE *SteamAPI_ISteamInput_GetInputTypeForHandle)(ISteamInput* input,  InputHandle_t inputHandle);
+
+#define STIK_INPUT_MAX_COUNT (16)
+#define STIK_MAX_ORIGINS (8)
 
 
 struct Atheneum;
 
+typedef struct StikFunctions {
+    SteamAPI_SteamInput_v006 createInputInstanceV006;
+    SteamAPI_ISteamInput_Init init;
+    SteamAPI_ISteamInput_GetActionSetHandle getActionSetHandle;
+    SteamAPI_ISteamInput_GetDigitalActionHandle getDigitalActionHandle;
+    SteamAPI_ISteamInput_ActivateActionSet activateActionSet;
+    SteamAPI_ISteamInput_GetConnectedControllers getConnectedControllers;
+    SteamAPI_ISteamInput_RunFrame runFrame;
+    SteamAPI_ISteamInput_GetDigitalActionOrigins getDigitalActionOrigins;
+    SteamAPI_ISteamInput_GetDigitalActionData getDigitalActionData;
+    SteamAPI_ISteamInput_GetCurrentActionSet getCurrentActionSet;
+    SteamAPI_ISteamInput_GetInputTypeForHandle getInputTypeForHandle;
+    SteamAPI_ISteamInput_GetControllerForGamepadIndex getControllerForGamepadIndex;
+} StikFunctions;
+
 typedef struct Stik {
     ISteamInput* input;
+    StikFunctions functions;
 } Stik;
 
 int stikInit(Stik* self, struct Atheneum *atheneum);
+int stikGetConnectedControllers(Stik* self, InputHandle_t* handles);
 
 #endif
--- src/lib/input.c
+++ src/lib/input.c
@@ -2,99 +2,98 @@
 #include <atheneum/atheneum.h>
 #include <stik/stik.h>
 
-typedef ISteamInput* (S_CALLTYPE *SteamAPI_SteamInput_v006)(void);
-typedef SteamBool (S_CALLTYPE *SteamAPI_ISteamInput_Init)(ISteamInput* input, SteamBool explicitRunFrame);
-typedef SteamU64 (S_CALLTYPE *SteamAPI_ISteamInput_GetActionSetHandle)(ISteamInput* input, SteamConstantString actionSet);
-typedef SteamU64 (S_CALLTYPE *SteamAPI_ISteamInput_GetDigitalActionHandle)(ISteamInput* input, SteamConstantString digitalAction);
-typedef SteamU64 (S_CALLTYPE *SteamAPI_ISteamInput_GetControllerForGamepadIndex)(ISteamInput* input, SteamInt controllerIndex);
-typedef void (S_CALLTYPE *SteamAPI_ISteamInput_ActivateActionSet)(ISteamInput* input, SteamU64 inputHandle, SteamU64 actionSetHandle);
-typedef SteamInt (S_CALLTYPE *SteamAPI_ISteamInput_GetConnectedControllers)(ISteamInput* input, SteamU64 *handlesOut);
-typedef void (S_CALLTYPE *SteamAPI_ISteamInput_RunFrame)(ISteamInput* input);
-typedef SteamInt EInputActionOrigin;
-
-typedef  SteamInt (S_CALLTYPE *SteamAPI_ISteamInput_GetDigitalActionOrigins)(ISteamInput* input, SteamU64 inputHandle, SteamU64 actionSetHandle, SteamU64 digitalActionHandle, EInputActionOrigin *originsOut );
 
 
 
-typedef struct InputDigitalActionData_t
+
+
+
+
+static int stikInitFunctions(StikFunctions* self, Atheneum* atheneum)
 {
-#pragma pack(push, 0)
-    SteamBool state;
-    SteamBool active;
-#pragma pack(pop)
-} InputDigitalActionData_t;
-
-#define STEAM_INPUT_MAX_COUNT (16)
-
-typedef InputDigitalActionData_t (S_CALLTYPE *SteamAPI_ISteamInput_GetDigitalActionData)(ISteamInput* input, SteamU64 controllerHandle, SteamU64 digitalActionHandle);
-
-int stikInit(Stik* self, Atheneum *atheneum)
-{
-    SteamAPI_SteamInput_v006 createInput = (SteamAPI_SteamInput_v006) atheneumAddress(atheneum, "SteamAPI_SteamInput_v006");
-    ISteamInput inputInstance = createInput();
-
-    CLOG_INFO("stik create %p", inputInstance);
-
-    SteamAPI_ISteamInput_Init initInput = (SteamAPI_ISteamInput_Init) atheneumAddress(atheneum, "SteamAPI_ISteamInput_Init");
-    SteamBool initWorked = initInput(inputInstance, 0);
-
-    CLOG_INFO("stik init %d", initWorked);
-
-
-    SteamAPI_ISteamInput_GetActionSetHandle getActionSetHandle = (SteamAPI_ISteamInput_GetActionSetHandle) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetActionSetHandle");
-    SteamU64 setHandle = getActionSetHandle(inputInstance, "Gamepad");
-    CLOG_INFO("stik getHandle %p", setHandle);
-    if (setHandle == 0) {
-        return -4;
-    }
-
-    SteamAPI_ISteamInput_RunFrame runFrame = (SteamAPI_ISteamInput_RunFrame) atheneumAddress(atheneum, "SteamAPI_ISteamInput_RunFrame");
-    runFrame(inputInstance);
-    CLOG_INFO("stik runhandle");
-
-    SteamAPI_ISteamInput_GetConnectedControllers getConnectedControllers = (SteamAPI_ISteamInput_GetConnectedControllers) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetConnectedControllers");
-    SteamU64 controllerHandles[STEAM_INPUT_MAX_COUNT];
-    SteamInt controllerCount = getConnectedControllers(inputInstance, controllerHandles);
-
-    if (controllerCount <= 0) {
-        return -3;
-    }
-
-    SteamU64 controllerHandle = controllerHandles[0];
-    CLOG_INFO("stik controllerHandle %08X", controllerHandle);
-
-    runFrame(inputInstance);
-    CLOG_INFO("stik runhandle2");
-
-    SteamAPI_ISteamInput_ActivateActionSet activateActionSet = (SteamAPI_ISteamInput_ActivateActionSet) atheneumAddress(atheneum, "SteamAPI_ISteamInput_ActivateActionSet");
-    activateActionSet(inputInstance, controllerHandle, setHandle);
-
-    runFrame(inputInstance);
-    CLOG_INFO("stik runhandle2");
-
-
-
-    SteamAPI_ISteamInput_GetDigitalActionHandle getDigitalActionHandle = (SteamAPI_ISteamInput_GetDigitalActionHandle) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetDigitalActionHandle");
-    SteamU64 abilityOneHandle = getDigitalActionHandle(inputInstance, "Interact");
-    CLOG_INFO("stik abilityOneHandle %d", abilityOneHandle);
-    if (abilityOneHandle == 0) {
-    //    return -5;
-    }
-
-#define MAX_ORIGINS (8)
-
-    EInputActionOrigin origins[MAX_ORIGINS];
-    SteamAPI_ISteamInput_GetDigitalActionOrigins getDigitalActionOrigins = (SteamAPI_ISteamInput_GetDigitalActionOrigins) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetDigitalActionOrigins");
-    SteamInt numberOfOrigins = getDigitalActionOrigins(inputInstance, controllerHandle, setHandle, abilityOneHandle, origins);
-    CLOG_INFO("stik numberOfOrigins %d", numberOfOrigins);
-
-    SteamAPI_ISteamInput_GetDigitalActionData getDigitalActionData = (SteamAPI_ISteamInput_GetDigitalActionData) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetDigitalActionData");
-    InputDigitalActionData_t data = getDigitalActionData(inputInstance, controllerHandle, abilityOneHandle);
-
-    CLOG_INFO("stik digital action %d %d", data.state, data.active);
-    if (!data.active) {
-        return -904;
-    }
+    self->createInputInstanceV006 = (SteamAPI_SteamInput_v006) atheneumAddress(atheneum, "SteamAPI_SteamInput_v006");;
+    self->init  = (SteamAPI_ISteamInput_Init) atheneumAddress(atheneum, "SteamAPI_ISteamInput_Init");
+    self->getActionSetHandle = (SteamAPI_ISteamInput_GetActionSetHandle) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetActionSetHandle");
+    self->getDigitalActionHandle  = (SteamAPI_ISteamInput_GetDigitalActionHandle) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetDigitalActionHandle");
+    self->activateActionSet  = (SteamAPI_ISteamInput_ActivateActionSet) atheneumAddress(atheneum, "SteamAPI_ISteamInput_ActivateActionSet");
+    self->getConnectedControllers = (SteamAPI_ISteamInput_GetConnectedControllers) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetConnectedControllers");
+    self->runFrame  = (SteamAPI_ISteamInput_RunFrame) atheneumAddress(atheneum, "SteamAPI_ISteamInput_RunFrame");
+    self->getDigitalActionOrigins  = (SteamAPI_ISteamInput_GetDigitalActionOrigins) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetDigitalActionOrigins");
+    self->getDigitalActionData = (SteamAPI_ISteamInput_GetDigitalActionData) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetDigitalActionData");
+    self->getCurrentActionSet = (SteamAPI_ISteamInput_GetCurrentActionSet) atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetCurrentActionSet");
+    self->getInputTypeForHandle = (SteamAPI_ISteamInput_GetInputTypeForHandle)atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetInputTypeForHandle");
+    self->getControllerForGamepadIndex =  (SteamAPI_ISteamInput_GetControllerForGamepadIndex)atheneumAddress(atheneum, "SteamAPI_ISteamInput_GetControllerForGamepadIndex");
 
     return 0;
 }
+
+
+int stikInit(Stik* self, Atheneum *atheneum)
+{
+    int functionsResult = stikInitFunctions(&self->functions, atheneum);
+    if (functionsResult < 0) {
+        return functionsResult;
+    }
+
+    self->input = self->functions.createInputInstanceV006();
+
+    CLOG_INFO("stik create %p", self->input);
+
+    SteamBool initWorked = self->functions.init(self->input);
+    if (!initWorked) {
+        return -6;
+    }
+//    self->functions.runFrame(self->input);
+
+    CLOG_INFO("stik init %d", initWorked);
+
+    return 0;
+}
+
+int stikGetConnectedControllers(Stik* self, InputHandle_t* handles)
+{
+    return self->functions.getConnectedControllers(self->input, handles);
+}
+
+/*
+ *
+
+self->functions.runFrame(self->input);
+CLOG_INFO("stik runhandle");
+
+InputHandle_t controllerHandles[STEAM_INPUT_MAX_COUNT];
+SteamInt controllerCount = self->functions.getConnectedControllers(self->input, controllerHandles);
+
+if (controllerCount <= 0) {
+return -3;
+}
+
+InputHandle_t controllerHandle = controllerHandles[0];
+CLOG_INFO("stik controllerHandle %08X", controllerHandle);
+
+self->functions.runFrame(self->input);
+CLOG_INFO("stik runhandle2");
+
+self->functions.activateActionSet(self->input, controllerHandle, setHandle);
+
+self->functions.runFrame(self->input);
+CLOG_INFO("stik runhandle2");
+
+
+
+
+EInputActionOrigin origins[MAX_ORIGINS];
+SteamInt numberOfOrigins = self->functions.getDigitalActionOrigins(self->input, controllerHandle, setHandle, abilityOneHandle, origins);
+CLOG_INFO("stik numberOfOrigins %d", numberOfOrigins);
+
+self->functions.runFrame(self->input);
+
+InputDigitalActionData_t data = self->functions.getDigitalActionData(self->input, controllerHandle, abilityOneHandle);
+
+CLOG_INFO("stik digital action %d %d", data.state, data.active);
+if (!data.active) {
+return -904;
+}
+
+
+ */

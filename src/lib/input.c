91ad85cff392b38a0e1845e60501bf7745231cc4
Peter Bjorklund <piot@hotmail.com>
2023-08-08 16:52:37 +0200

    chore: missing header

--- src/lib/input.c
+++ src/lib/input.c
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #include "clog/clog.h"
 #include <atheneum/atheneum.h>
 #include <stik/stik.h>
